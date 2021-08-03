dd9a23c1e74c15455366d17338117bb30cd0f109
cbh34680 <cbh34680@gmail.com>
2021-08-03 14:15:08 +0900

    test

--- lib/batchq/handlers/simple.py
+++ lib/batchq/handlers/simple.py
@@ -240,8 +240,10 @@ async def handle_world_end(taskid:int, peername, worker:typing.Dict, exec_params
         other_hosts = ( v for v in active_hosts.values() if v['hostid'] != my_hostid )
 
         for host in other_hosts:
+            peername = host['peername']
+
             message = {
-                'peername': host['peername'],
+                'peername': peername,
                 'payload': {
                     'worker-key': 'world-end',
                     'exec-params': {
@@ -254,6 +256,8 @@ async def handle_world_end(taskid:int, peername, worker:typing.Dict, exec_params
 
             queue = memory.get_queue('batchq.postman')
             await queutil.put(queue, message, where=here())
+
+            logger.info(f'propagate peername={peername}')
 
     logger.info(f'peer={peername}) receive world-end, shutdown local 1 sec later')
     loop = asyncio.get_running_loop()
